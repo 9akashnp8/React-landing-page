2c33389095ae049a3ce1780e92d1b990e01edc81
Akash NP <akashnp1998@gmail.com>
2022-10-02 00:55:29 +0530

    Make CourseCard Component Resulable by setting up props
    Setting up props enables reusing of the CourseCard elements, we only
    need to pass the title and description for the card.
    
    Looping through an object of course details would be the next improvement.

--- src/Section2.js
+++ src/Section2.js
@@ -12,24 +12,25 @@ const primaryColor = "#F83D5C";
 // const breakpoints = ['0em', <small> '30em', <mobile> '48em', <laptop> '62em', <desktop> '80em', <bigscreen> '96em']
 //  [small, mobile, laptop, desktop, bigscreen]
 
-function CourseCard() {
+function CourseCard({course, description}) {
     return (
         <Box bgColor='white'>
             <Image boxSize='200px' src={courseCardPlaceholder} alt='Dan Abramov' />
-            <Heading>CA Foundation</Heading>
-            <Text>400+ Hours Chartered Accountancy Foundation Course Recorded Lecture.</Text>
+            <Heading>{course}</Heading>
+            <Text>{description}</Text>
         </Box>
     )
 }
 
 export default function ThreeColumnCourses() {
+
     return (
       <Grid gap={5} gridAutoFlow={['row', 'row', 'column']} gridAutoColumns='1fr' placeItems='center' px={['1rem', '2rem', '4rem', '8rem']}
         bgColor={primaryColor}
       >
-        <CourseCard/>
-        <CourseCard/>
-        <CourseCard/>
+        <CourseCard course='CA Foundation' description='400+ Hours Chartered Accountancy Foundation Course Recorded Lecture.'/>
+        <CourseCard course='CMA Foundation' description='190+ Hours Cost and Management Accountant Recorded Lecture.'/>
+        <CourseCard course='CSEET' description='140+ Hours CS Executive Entrance Test Recorded Lecture.'/>
       </Grid>
     )
   }
