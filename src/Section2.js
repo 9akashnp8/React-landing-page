6ec5e875d46aa405e32f817070845d81a09b6546
Akash NP <akashnp1998@gmail.com>
2022-10-02 01:12:44 +0530

    Update ThreeColumnCourses component to render CourseCard from Course Object

--- src/Section2.js
+++ src/Section2.js
@@ -12,10 +12,16 @@ const primaryColor = "#F83D5C";
 // const breakpoints = ['0em', <small> '30em', <mobile> '48em', <laptop> '62em', <desktop> '80em', <bigscreen> '96em']
 //  [small, mobile, laptop, desktop, bigscreen]
 
+const courseDatas = [
+  {id: 1, title: 'CA Foundation', description: '400+ Hours Chartered Accountancy Foundation Course Recorded Lecture.'},
+  {id: 2, title: 'CMA Foundation', description: '190+ Hours Cost and Management Accountant Recorded Lecture.'},
+  {id: 3, title: 'CSEET', description: '140+ Hours CS Executive Entrance Test Recorded Lecture.'},
+]
+
 function CourseCard({course, description}) {
     return (
         <Box bgColor='white'>
-            <Image boxSize='200px' src={courseCardPlaceholder} alt='Dan Abramov' />
+            <Image boxSize='200px' src={courseCardPlaceholder} alt='' />
             <Heading>{course}</Heading>
             <Text>{description}</Text>
         </Box>
@@ -24,13 +30,19 @@ function CourseCard({course, description}) {
 
 export default function ThreeColumnCourses() {
 
-    return (
-      <Grid gap={5} gridAutoFlow={['row', 'row', 'column']} gridAutoColumns='1fr' placeItems='center' px={['1rem', '2rem', '4rem', '8rem']}
-        bgColor={primaryColor}
-      >
-        <CourseCard course='CA Foundation' description='400+ Hours Chartered Accountancy Foundation Course Recorded Lecture.'/>
-        <CourseCard course='CMA Foundation' description='190+ Hours Cost and Management Accountant Recorded Lecture.'/>
-        <CourseCard course='CSEET' description='140+ Hours CS Executive Entrance Test Recorded Lecture.'/>
-      </Grid>
+  let courseCardList = []
+
+  courseDatas.forEach((courseData) => {
+    courseCardList.push(
+      <CourseCard course={courseData.title} description={courseData.description}/>
     )
-  }
+  })
+
+  return (
+    <Grid gap={5} gridAutoFlow={['row', 'row', 'column']} gridAutoColumns='1fr' placeItems='center' px={['1rem', '2rem', '4rem', '8rem']}
+      bgColor={primaryColor}
+    >
+      {courseCardList}
+    </Grid>
+  )
+}
